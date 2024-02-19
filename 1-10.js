dd62dd810f6e2287d6553c0cb934c37d8b17e84e
s.sernald <fekke4201337@gmail.com>
2024-02-19 13:55:49 +0100

    update

--- 1-10.js
+++ 1-10.js
@@ -5,6 +5,8 @@ console.log('What do you want to do? Enter a number.');
 console.log('1. Square root of: ?');
 console.log('2. Salesman salary');
 console.log('3. Hours to minutes and seconds');
+console.log('4. Add 3 numbers and average number');
+console.log('5. Convert SEK to pounds and dollars');
 
 let menyOption = prompt('Enter your option: ');
 
@@ -32,6 +34,16 @@ switch (menyOption) {
     console.log('You chose option 3');
     hoursToMins();
     break;
+
+    case "4":
+      console.log('You chose option 4');
+      add3Average();
+      break;
+
+      case "5":
+        console.log('You chose option 5');
+        convertCurrency();
+        break;
 
   default:
     console.log('You chose nothing!');
@@ -69,4 +81,52 @@ function hoursToMins() {
   let resultSeconds = resultMinutes * 60;
 
   console.log(`In ${number} hours there is ${resultMinutes} minutes and ${resultSeconds} seconds.`)
+}
+
+function add3Average() {
+  const number1 = prompt('Enter number 1:');
+
+  if (!isNaN(number1)) {
+    console.log('You entered a valid option!')
+  } else {
+    console.log('You entered a unvalid number. Program closing!')
+  }
+
+  const number2 = prompt('Enter number 2:');
+
+  if (!isNaN(number2)) {
+    console.log('You entered a valid option!')
+  } else {
+    console.log('You entered a unvalid number. Program closing!')
+  }
+
+  const number3 = prompt('Enter number 3:');
+
+  if (!isNaN(number3)) {
+    console.log('You entered a valid option!')
+  } else {
+    console.log('You entered a unvalid number. Program closing!')
+  }
+
+  totalNumber = parseInt(number1) + parseInt(number2) + parseInt(number3);
+ 
+  let avgNumber = totalNumber / 3;
+
+  console.log(`The total number is ${totalNumber} and the average number is ${avgNumber}`);
+}
+
+function convertCurrency() {
+  console.log(`What amount of SEK do you want to convert into pounds and dollars?`);
+  const number = prompt(`Enter SEK here: `);
+
+  if (!isNaN(number)) {
+    console.log('You entered a valid option!')
+  } else {
+    console.log('You entered a unvalid number. Program closing!')
+  }
+
+  numberPound = parseInt(number) / 10;
+  numberDollar = parseInt(number) / 6;
+
+  console.log(`${number} is equals ${numberPound} in pounds and ${numberDollar} in dollars`);
 }
